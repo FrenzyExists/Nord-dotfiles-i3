1314b8b06b242e9e2452f8ce34e8ead25cb73b0e
FrenzyExists <frenzyexists@gmail.com>
2021-03-09 10:02:06 -0400

    Got eww boys! Btw, yeah i know, its messy

---
+++ config/eww/scripts/launcher.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+# >> Created and tested on : rofi 1.6.0-1
+
+theme="style"
+
+dir="$HOME/.config/rofi/launcher"
+
+rofi -no-lazy-grab -show drun -modi run,drun,window -theme /$HOME/.config/rofi/minimal.rasi
